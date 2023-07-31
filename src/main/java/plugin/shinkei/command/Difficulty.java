690d55517252ac08f62bf7f279277d95306e6f00
yoshi-koyama <koyama@reytech.co.jp>
2023-07-31 10:59:30 +0900

    インデント修正

--- src/main/java/plugin/shinkei/command/Difficulty.java
+++ src/main/java/plugin/shinkei/command/Difficulty.java
@@ -6,37 +6,37 @@ package plugin.shinkei.command;
  */
 public enum Difficulty {
 
-    EASY("easy", 15, 1, 3),
-    NORMAL("normal", 60, 3, 15),
-    HARD("hard", 120, 5, 35),
-    NONE("none", 0, 0, 0);
+  EASY("easy", 15, 1, 3),
+  NORMAL("normal", 60, 3, 15),
+  HARD("hard", 120, 5, 35),
+  NONE("none", 0, 0, 0);
 
 
-    private String difficulty;
-    private int gameTime;
-    private int loopCount_entityPosition;
-    private int loopCount_dummyList;
+  private String difficulty;
+  private int gameTime;
+  private int loopCount_entityPosition;
+  private int loopCount_dummyList;
 
-    Difficulty(String difficulty, int gameTime, int loopCount_entityPosition, int loopCount_dummyList) {
-        this.difficulty = difficulty;
-        this.gameTime = gameTime;
-        this.loopCount_entityPosition = loopCount_entityPosition;
-        this.loopCount_dummyList = loopCount_dummyList;
-    }
+  Difficulty(String difficulty, int gameTime, int loopCount_entityPosition, int loopCount_dummyList) {
+    this.difficulty = difficulty;
+    this.gameTime = gameTime;
+    this.loopCount_entityPosition = loopCount_entityPosition;
+    this.loopCount_dummyList = loopCount_dummyList;
+  }
 
-    public String preparedDifficulty() {
-        return difficulty;
-    }
+  public String preparedDifficulty() {
+    return difficulty;
+  }
 
-    public int getGameTime() {
-        return gameTime;
-    }
+  public int getGameTime() {
+    return gameTime;
+  }
 
-    public int getLoopCount_entityPosition() {
-        return loopCount_entityPosition;
-    }
+  public int getLoopCount_entityPosition() {
+    return loopCount_entityPosition;
+  }
 
-    public int getLoopCount_dummyList() {
-        return loopCount_dummyList;
-    }
+  public int getLoopCount_dummyList() {
+    return loopCount_dummyList;
+  }
 }
